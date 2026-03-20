3de44984e2ee751e848abc9b0e39773ad7e57fe2
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:06:08 +0700

    Bai3 upgrade

---
+++ Bai3.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("nhap so phan tu");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap so phan tu" + (i + 1));
+    a[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Danh sach:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i]+" ");
+}
+Console.WriteLine();
+int max = a[0];
+for (int i = 0;i < n; i++)
+{
+    if (a[i] > max)
+        max = a[i]; 
+}
+Console.WriteLine("so nguyen lon nhat la "+ max);
