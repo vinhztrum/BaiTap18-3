fda1a735361e062527fd3ab0c4adc6288ee4dba0
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:15:23 +0700

    Bai15 upgrade

---
+++ Bai15.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Dictionary<string, string> dict = new Dictionary<string, string>();
+Console.Write("Nhap so tu: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Nhap tu Anh: ");
+    string eng = Console.ReadLine();
+
+    Console.Write("Nhap nghia: ");
+    string vie = Console.ReadLine();
+
+    dict[eng] = vie;
+}
+Console.WriteLine();
+Console.Write("Nhap tu can tra: ");
+string key = Console.ReadLine();
+
+if (dict.ContainsKey(key))
+{
+    Console.WriteLine("Nghia: " + dict[key]);
+}
+else
+{
+    Console.WriteLine("Khong tim thay!");
+}
