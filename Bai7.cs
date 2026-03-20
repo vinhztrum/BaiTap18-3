563b5b6eb3f2cb224ab01c8bb4b43af290719449
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:07:58 +0700

    Bai7 upgrade

---
+++ Bai7.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("nhap so phan tu");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap so phan tu" + (i + 1));
+    a[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Danh sach:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i]+(" "));
+}
+for (int i = 0;i < n/2; i++)
+
+    {
+       
+        
+            int swap = a[i];
+            a[i] = a[n-i-1];
+            a[n-i-1] = swap;
+        
+    };
+Console.WriteLine();
+Console.WriteLine("day sau dao nguoc ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i]+ (" "));
+}
