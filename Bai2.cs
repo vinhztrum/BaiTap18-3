2f28556e90094997d78559df8ffe2c14c5f93146
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:05:22 +0700

    Bai2 upgrade

---
+++ Bai2.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("nhap so phan tu");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap so phan tu" + (i + 1));
+    a[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Danh sach:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i] +" ");
+}
+Console.WriteLine();
+int s = 0;
+for (int i = 0; i < n; i++)
+{
+    s = s + a[i];
+}
+Console.Write("Tong cac so nguyen la " + s);
