ec69648fdb9de5e8c8c11c312eeed452d985f1a9
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:06:43 +0700

    Bai4 upgrade

---
+++ Bai4.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("nhap so phan tu");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap so phan tu" + (i + 1));
+    a[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Danh sach:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i] + (" "));
+}
+int dem = 0;
+for (int i = 0; i < n; i++)
+    if (a[i]% 2  ==0)
+    {
+        dem++; 
+    }
+Console.WriteLine("so phan tu chan la "+dem);
