1d83457831d3e4f5c4531fbe47591dbba9cbe8d8
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:16:09 +0700

    Bai17 upgrade

---
+++ Bai17.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("Nhap so phan tu");
+int n=int.Parse(Console.ReadLine());
+int[] a=new int[n];
+for(int i=0; i<n; i++)
+{
+    Console.WriteLine("Nhap phan tu thu "+(i+1));
+    a[i]=int.Parse(Console.ReadLine());
+}
+int so = a[0];
+
+int demnhieu = 0;
+for(int i = 0; i < n; i++) { 
+    int dem = 0;
+for (int j=0;j<n;j++)
+    {
+        
+        if (a[i]==a[j])
+            dem=dem+1;
+        if (dem > demnhieu)
+        {
+            demnhieu = dem;
+            so = a[i];
+        }
+    }
+}
+Console.WriteLine("So xuat hien nhieu nhat la " + so);
+
+Console.WriteLine("Voi so lan xuat hien la " + demnhieu);
+
