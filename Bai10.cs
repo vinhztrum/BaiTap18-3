e3c923dba007c2dc7c6c92c14df5de59830f1a51
thanhvinh <vinhyhan@gmail.com>
2026-03-20 21:11:29 +0700

    Bai10 upgrade

---
+++ Bai10.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("PhamTranThanhVinh");
+Console.WriteLine("2415053122248");
+Console.WriteLine("nhap so phan tu");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap so phan tu" + (i + 1));
+    a[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Danh sach:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i] + (" "));
+}
+
+int min  = a[0];
+for  (int i = 0; i < n-1; i++)
+    {
+        if (a[i] < min)
+            min = a[i];
+            
+    }
+Console.WriteLine();    
+Console.WriteLine("so nho nhat day la "+min);
